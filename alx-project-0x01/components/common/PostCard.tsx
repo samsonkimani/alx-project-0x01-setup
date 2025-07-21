e8b7eea8c72869d0620724c4db5415c651009b15
samsonkimani <samsonkimani43@gmail.com>
2025-07-21 11:46:54 +0300

    routing next

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,9 @@
+import React  from "react";
+
+const PostCard:React.FC = () => {
+    return (
+        <h1>Postcard</h1>
+    )
+}
+
+export default PostCard;
---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Posts:React.FC = () => {
+    return (
+        <h1>Posts</h1>
+    )
+}
+
+export default Posts;
